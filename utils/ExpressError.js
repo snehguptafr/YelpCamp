d880c56f208be489c4771ce5a88e72664ce435a4
snehguptafr <guptasneh302004@gmail.com>
2024-01-23 22:14:00 +0530

    add error handling utils

---
+++ utils/ExpressError.js
@@ -0,0 +1,8 @@
+class ExpressError extends Error{
+    constructor(message, statusCode){
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ExpressError;
---
+++ utils/catchAsync.js
@@ -0,0 +1,7 @@
+const func = () => {
+    return (req, res, next) => {
+        func(req, res, next).catch(next)
+    }
+}
+
+module.exports = func;
