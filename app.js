fce09afe5dee02d9d5bf5cdedb31c1371bb8942e
snehguptafr <guptasneh302004@gmail.com>
2024-03-24 12:28:08 +0530

    separate campground routes

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const path = require("path");
 const app = express();
 const methodOverride = require("method-override");
 
+const campgrounds = require("./routes/campgrounds");
+
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 
@@ -23,17 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-const validateCampground = (req, res, next) => {
 
-  const { error } = campgroundSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  }
-  else{
-    next();
-  }
-}
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
@@ -46,43 +38,11 @@ const validateReview = (req, res, next) => {
   }
 }
 
+app.use('/campgrounds', campgrounds);
+
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
-});
-
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
-});
-
-app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
-  const camp = new Campground(req.body.campground);
-  await camp.save();
-  res.redirect(`/campgrounds/${camp._id}`);
-}));
-
-app.get("/campgrounds/:id",catchAsync( async (req, res, next) => {
-  const camp = await Campground.findById(req.params.id).populate('reviews');
-  res.render("campgrounds/show", { camp });
-}));
-
-app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const camp = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  res.redirect(`/campgrounds/${id}`);
-}));
-
-app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const camp = await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
-}));
 
 app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
   const camp = await Campground.findById(req.params.id);
---
+++ routes/campgrounds.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const Campground = require("../models/campground");
+const {campgroundSchema} = require("../schemas")
+
+
+const validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+      const msg = error.details.map(el => el.message).join(',');
+      throw new ExpressError(msg, 400);
+    }
+    else{
+      next();
+    }
+}
+
+router.get("/", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  });
+  
+router.get("/new", (req, res) => {
+    res.render("campgrounds/new");
+ });
+  
+router.post("/", validateCampground, catchAsync(async (req, res, next) => {
+    const camp = new Campground(req.body.campground);
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`);
+ }));
+  
+router.get("/:id",catchAsync( async (req, res, next) => {
+    const camp = await Campground.findById(req.params.id).populate('reviews');
+    res.render("campgrounds/show", { camp });
+ }));
+  
+router.put("/:id", validateCampground, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${id}`);
+ }));
+  
+router.delete("/:id", catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+ }));
+
+ module.exports = router;
