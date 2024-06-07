07a3734997fc410cca25da1ca36191a04b4e8c9f
snehguptafr <guptasneh302004@gmail.com>
2024-06-07 14:13:19 +0530

    add flash messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -36,6 +36,10 @@ router.post("/", validateCampground, catchAsync(async (req, res, next) => {
   
 router.get("/:id",catchAsync( async (req, res, next) => {
     const camp = await Campground.findById(req.params.id).populate('reviews');
+    if(!camp){
+      req.flash("error", "Campground not found");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { camp });
  }));
   
@@ -44,17 +48,23 @@ router.put("/:id", validateCampground, catchAsync(async (req, res) => {
     const camp = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash("success", "Successfully updated a campground")
     res.redirect(`/campgrounds/${id}`);
  }));
   
 router.delete("/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     const camp = await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground")
     res.redirect("/campgrounds");
  }));
 
 router.get("/:id/edit", catchAsync(async (req, res) => {
   const camp = await Campground.findById(req.params.id);
+  if(!camp){
+    req.flash("error", "Campground not found");
+    return res.redirect("/campgrounds");
+  }
   res.render("campgrounds/edit", { camp });
 }));
 
