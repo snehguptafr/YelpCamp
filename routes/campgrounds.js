86e04bd37832e50a3af1c17ebbe23708e972cda5
snehguptafr <guptasneh302004@gmail.com>
2024-06-19 22:04:43 +0530

    populate username in review

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,7 +22,9 @@ router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res, nex
  }));
   
 router.get("/:id",catchAsync( async (req, res, next) => {
-    const camp = await Campground.findById(req.params.id).populate('reviews').populate("author");
+    const camp = await Campground.findById(req.params.id)
+      .populate({path: "reviews", populate:{ path: "author" }})
+      .populate("author");
     if(!camp){
       req.flash("error", "Campground not found");
       return res.redirect("/campgrounds");
