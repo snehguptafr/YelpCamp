a2182338930a82c41c9dc34b37e886b592083c1f
snehguptafr <guptasneh302004@gmail.com>
2024-06-19 22:04:22 +0530

    add isReviewAuthor middleware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const Review = require("./models/review");
 const Campground = require("./models/campground");
 const { campgroundSchema, reviewSchema } = require("./schemas");
 const ExpressError = require("./utils/ExpressError");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
-        req.session.returnTo = req.originalUrl;
+        req.session.returnTo = req.query._method === 'DELETE' ? `/campgrounds/${id}` : req.originalUrl;
         req.flash("error", "You need to be signed in");
         return res.redirect("/login");
     }
@@ -44,6 +45,16 @@ module.exports.isAuthor = async(req, res, next) =>{
   const { id } = req.params;
   const camp = await Campground.findById(id);
     if(!camp.author.equals(req.user._id)){
+      req.flash("error", "You don't have permissions to do that!");
+      return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async(req, res, next) =>{
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
       req.flash("error", "You don't have permissions to do that!");
       return res.redirect(`/campgrounds/${id}`)
     }
