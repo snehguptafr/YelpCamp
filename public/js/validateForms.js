e275660058487ce413ab10f7ee7dbcd80061567e
snehguptafr <guptasneh302004@gmail.com>
2024-06-27 16:55:38 +0530

    minor changes

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -1,6 +1,5 @@
 (function () {
     'use strict'
-    bsCustomFileInput.init()
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.validated-form')
   
