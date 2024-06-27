c9d403111b1ce738a9806db87b56ba1761816381
snehguptafr <guptasneh302004@gmail.com>
2024-06-27 16:54:45 +0530

    schema changes

--- models/campground.js
+++ models/campground.js
@@ -6,12 +6,27 @@ const ImageSchema = new Schema({
     url: String,
     filename: String
 })
+
 ImageSchema.virtual("thumbnail").get(function(){
     return this.url.replace("/upload", "/upload/w_200")
 })
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
@@ -25,7 +40,17 @@ const CampgroundSchema = new Schema({
             ref: "Review"
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
+CampgroundSchema.virtual("type").get(function() {
+    return 'Feature'
+})
 
 CampgroundSchema.post(('findOneAndDelete'), async (doc) => {
     await Review.deleteMany({_id: {$in : doc.reviews }})
