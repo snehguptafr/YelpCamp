9f331865d1658a0ad6084a27e17282e1c0276c7a
snehguptafr <guptasneh302004@gmail.com>
2024-06-28 17:59:13 +0530

    security + mongodb atlas connection

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const path = require("path");
 const app = express();
 const methodOverride = require("method-override");
 const passport = require("passport");
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+const MongoStore = require('connect-mongo');
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
@@ -18,10 +21,10 @@ const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews")
 const users = require("./routes/users")
 
-const ExpressError = require("./utils/ExpressError");
+const connectionUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/yelpcamp")
+  .connect(connectionUrl)
   .then(() => console.log("Connected to the database"))
   .catch(() => console.log("Error connecting to the db"));
 
@@ -31,13 +34,27 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize({ replaceWith: "_"}))
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+  mongoUrl: connectionUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+      secret,
+  }
+});
 
 const sessionConfig = {
-  secret: "topsecret",
+  store,
+  name: "session",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie:{
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
@@ -45,6 +62,49 @@ const sessionConfig = {
 
 app.use(session(sessionConfig)); //should be before passport session
 app.use(flash());
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.maptiler.com/",
+  "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+  "https://cdn.maptiler.com/",
+  "https://api.maptiler.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/ddqlbimns/", 
+              "https://images.unsplash.com/",
+              "https://api.maptiler.com/",
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -67,20 +127,14 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-
-
-
-
-// app.all('*', (req, res, next) => {
-//   next(new ExpressError("Page not found", 404));
-// })
-
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if(!err.message) err.message = "Uh oh, something went wrong :("
   res.status(statusCode).render("error", { err })
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server up on port 3000");
 });
